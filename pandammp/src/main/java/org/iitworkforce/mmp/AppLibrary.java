f365213a31c4748a2928b11de264cde83f576339
sudheer <sudheer694@gmail.com>
2024-05-21 07:25:21 +0530

    MYSQL Connection Logic

--- pandammp/src/main/java/org/iitworkforce/mmp/AppLibrary.java
+++ pandammp/src/main/java/org/iitworkforce/mmp/AppLibrary.java
@@ -3,6 +3,12 @@ package org.iitworkforce.mmp;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Month;
@@ -22,7 +28,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class AppLibrary {
-	
+
 	public static String getDifferenceinYears(Integer dobYear)
 	{
 		Calendar cal = Calendar.getInstance();
@@ -109,10 +115,56 @@ public class AppLibrary {
 		return inputData;
 
 	}
-    public static int calculateAge(LocalDate birthdate, LocalDate currentDate) { 
-        // Calculate period between birthdate and current date 
-          Period period = Period.between(birthdate, currentDate); 
-          return period.getYears(); 
-    } 
+	public static int calculateAge(LocalDate birthdate, LocalDate currentDate) { 
+		// Calculate period between birthdate and current date 
+		Period period = Period.between(birthdate, currentDate); 
+		return period.getYears(); 
+	} 
+	public static String[][] getDBValues(String uname,String pword,String dbname,String tableName,String hostip) throws ClassNotFoundException, SQLException 
+	{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		//	Driver driver = new Driver();
+		/*
+		 * url a database url of the form jdbc:subprotocol:subnameuser 
+		 * the database user on whose behalf the connection is being madepassword 
+		 * the user's password
+		 */
+		String url="jdbc:mysql://"+hostip+":3306/"+dbname;
+		String username=uname;
+		String password=pword;
+
+		Connection con = DriverManager.getConnection(url, username, password);
+		Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+		//int  value = stmt.executeUpdate("INSERT INTO `mmp`.`patient_data` VALUES (11,'James','22/11/2021');");
+		//System.out.println("The rows are updated "+ value);
+
+		ResultSet rs =  stmt.executeQuery("Select * from "+dbname+"."+tableName);
+		rs.last();
+
+		int rows = rs.getRow();
+		System.out.println("Number of rows " + rows);
+		ResultSetMetaData rsmd = rs.getMetaData();
+		int cols = rsmd.getColumnCount();
+		System.out.println("Number of cols: "+ cols);
+
+		String data[][]= new String[rows][cols];
+		int i=0;
+		rs.first();
+		while(rs.next())
+		{
+			for(int j=0;j<cols;j++)
+			{
+
+				data[i][j]=rs.getString(j+1);
+				System.out.println(data[i][j]);
+				System.out.println("i :::"  + i +"@@@@"+"j:::::" + j);
+
+			}
+			i++;
+		}	
+		return data;
+	}
+
 
 }
