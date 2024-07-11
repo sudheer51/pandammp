0e7b43307e8281ee887b9d7574e1c926edb56e27
sudheer <sudheer694@gmail.com>
2024-07-11 07:23:19 +0530

    Updated the TOML file configuration

--- pandammp/src/test/java/org/iitworkforce/mmp/patientmodule/tests/GridExampleConfig.java
+++ pandammp/src/test/java/org/iitworkforce/mmp/patientmodule/tests/GridExampleConfig.java
@@ -6,34 +6,27 @@ import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class GridExampleConfig {
-	  DesiredCapabilities caps;
-
-	@BeforeMethod
-	public   void init()
-	{
-		caps = new DesiredCapabilities();
-		 
-	}
+	  DesiredCapabilities caps1,caps2;
 	@Test
 	public void executeTest6() throws MalformedURLException {
 		System.out.println("in test 6");
-		caps.setBrowserName("chrome");
-		caps.setVersion("113");
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), caps);
+		caps1 = new DesiredCapabilities();
+		caps1.setBrowserName("chrome");
+		caps1.setVersion("86");
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), caps1);
 		driver.get("http://www.google.com");
 		driver.quit();
 	}
 	@Test
 	public void executeTest2() throws MalformedURLException {
 		System.out.println("in test 2");
-
-		caps.setBrowserName("MicrosoftEdge");
-		caps.setVersion("126");
-		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), caps);
+		caps2 = new DesiredCapabilities();
+		caps2.setBrowserName("MicrosoftEdge");
+		caps2.setVersion("126");
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), caps2);
 		driver.get("http://www.google.com");
 		driver.quit();
 	}
